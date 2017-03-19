73dd9961ea81b228ede58e3812eea6338847c330
pshenglh <psheng@outlook.com>
2017-03-19 23:39:18 +0800

    简答的数据库模块

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    body_text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    com_text = models.TextField()
+    pub_date = models.DateField()
