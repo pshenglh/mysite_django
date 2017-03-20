931cb025f8a64ff73c3f9bc99c8dfe0726247bec
pshenglh <psheng@outlook.com>
2017-03-20 17:18:32 +0800

    删除文章和编辑文章

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^delete/(?P<blog_id>[0-9]+)/$', views.delete_blog, name='delete'),
+    url(r'^edit/(?P<blog_id>[0-9])+/$', views.edit_blog, name='edit'),
     url(r'(?P<blog_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^add_blog/$', views.add_blog, name='add_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-from django.http import request, HttpResponse
+from django.shortcuts import render, redirect
 from .models import Blog, BlogForm, Author
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 
+
 # 首页处理视图函数
 def index(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-mod_date')
     return render(request, 'index.html', {'blogs':blogs})
 
 #博客详细页面
@@ -14,18 +14,47 @@ def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'detail.html', {'blog':blog})
 
+#写文章
 def add_blog(request):
     if request.method == 'POST':
         form = BlogForm(request.POST)
         if form.is_valid():
-            b = Blog(title=form.cleaned_data['title'],
+            #将提交的数据保存到数据库中
+            blog = Blog(title=form.cleaned_data['title'],
                      body_text=form.cleaned_data['body_text'],
                      pub_date=timezone.now(),
                      mod_date=timezone.now(),
                      author=Author.objects.get(pk=1))
-            b.save()
-            render(request, 'index.html')
+            blog.save()
+            return redirect('index')
     else:
         form = BlogForm()
 
-    return render(request, 'addblog.html', {'form':form})
+    return render(request, 'add_blog.html', {'form':form})
+
+#删除文章
+def delete_blog(request, blog_id):
+    blog = Blog.objects.get(pk=blog_id)
+    blog.delete()
+    return redirect('index')
+
+#编辑文章
+def edit_blog(request, blog_id):
+    blog = Blog.objects.get(pk=blog_id)
+    data = {
+        'title': blog.title,
+        'body_text': blog.body_text
+    }
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            #修改该文章被修改的部分并将提交的数据保存到数据库中
+            blog.title = form.cleaned_data['title']
+            blog.body_text = form.cleaned_data['body_text']
+            blog.mod_date = timezone.now()
+            blog.save()
+            return redirect('index')
+    else:
+        form = BlogForm(data)
+
+    return render(request, 'add_blog.html', {'form':form})
